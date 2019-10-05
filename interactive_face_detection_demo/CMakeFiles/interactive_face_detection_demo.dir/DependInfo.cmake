c3e3bbc78066f771417531d83c68d0c40e8dd42f
weikaisu <37170449+weikaisu@users.noreply.github.com>
2019-10-05 17:17:07 +0800

    Add files via upload

---
+++ interactive_face_detection_demo/CMakeFiles/interactive_face_detection_demo.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/detectors.cpp" "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/CMakeFiles/interactive_face_detection_demo.dir/detectors.cpp.o"
+  "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/face.cpp" "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/CMakeFiles/interactive_face_detection_demo.dir/face.cpp.o"
+  "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/main.cpp" "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/CMakeFiles/interactive_face_detection_demo.dir/main.cpp.o"
+  "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/visualizer.cpp" "/home/sun/projects/EmotiIntelCPU/interactive_face_detection_demo/CMakeFiles/interactive_face_detection_demo.dir/visualizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  "HAVE_AVX2"
+  "HAVE_SSE"
+  "IE_THREAD=IE_THREAD_TBB"
+  "USE_OPENCV"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "interactive_face_detection_demo/../common"
+  "/opt/intel/openvino_2019.2.242/opencv/include"
+  "/opt/intel/openvino_2019.2.242/deployment_tools/inference_engine/include"
+  "/opt/intel/openvino_2019.2.242/deployment_tools/inference_engine/external/tbb/include"
+  "/opt/intel/openvino_2019.2.242/deployment_tools/inference_engine/src/extension"
+  "thirdparty/gflags/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/sun/projects/EmotiIntelCPU/ie_cpu_extension/CMakeFiles/ie_cpu_extension.dir/DependInfo.cmake"
+  "/home/sun/projects/EmotiIntelCPU/thirdparty/gflags/CMakeFiles/gflags_nothreads_static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
