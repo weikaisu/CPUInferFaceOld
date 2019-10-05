8755467bf6636c9549a9fa198695ae4f5e106205
weikaisu <37170449+weikaisu@users.noreply.github.com>
2019-10-05 17:11:10 +0800

    Add files via upload

---
+++ common/format_reader/CMakeFiles/format_reader.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sun/projects/EmotiIntelCPU/common/format_reader/MnistUbyte.cpp" "/home/sun/projects/EmotiIntelCPU/common/format_reader/CMakeFiles/format_reader.dir/MnistUbyte.cpp.o"
+  "/home/sun/projects/EmotiIntelCPU/common/format_reader/bmp.cpp" "/home/sun/projects/EmotiIntelCPU/common/format_reader/CMakeFiles/format_reader.dir/bmp.cpp.o"
+  "/home/sun/projects/EmotiIntelCPU/common/format_reader/format_reader.cpp" "/home/sun/projects/EmotiIntelCPU/common/format_reader/CMakeFiles/format_reader.dir/format_reader.cpp.o"
+  "/home/sun/projects/EmotiIntelCPU/common/format_reader/opencv_wraper.cpp" "/home/sun/projects/EmotiIntelCPU/common/format_reader/CMakeFiles/format_reader.dir/opencv_wraper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMPLEMENT_FORMAT_READER"
+  "USE_OPENCV"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "common/format_reader"
+  "common/format_reader/.."
+  "/opt/intel/openvino_2019.2.242/opencv/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
